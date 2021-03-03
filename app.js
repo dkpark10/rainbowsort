f2381a475cab057859c540b5c266bf3d816c91e6
nowinc10 <dkpark10@naver.com>
2021-03-03 22:23:15 +0900

    2021-03-03 rainbow color complete

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 const helmet = require('helmet');
 const compression = require('compression');
 const bodyparser = require('body-parser');
+const rainbowColor = require('./lib/createRainbow');
 
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
 
 const port = 8080;
-const sortrouter = require('./Router/sortrouter');
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
@@ -21,8 +21,21 @@ app.use(helmet());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use('/lib', express.static('lib'));
 
-app.use('/', sortrouter);
+app.use('/', (request, response) => {
+  const shuffledList = (function (len) {
+    return Array.from({ length: len }, (v, i) => (i + 1))
+      .map((element, curidx, arr) => {
+        const random = Math.floor(Math.random() * arr.length);
+        arr[curidx] = arr[random];
+        arr[random] = element;
+        return arr;
+      })[0]
+  })(rainbowColor.length);
+
+  response.render("index", { color: rainbowColor, shuffle: shuffledList });
+});
 
 app.use((request, response, next) => {
   response.status(404).send('Not Found');
